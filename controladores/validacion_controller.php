f908c8b8ca50326246f3cef11286856a509fef42
unknown <maacevedo2010@gmail.com>
2020-01-16 21:26:07 -0500

    Se corrige un error de pull, cambios OK

--- controladores/validacion_controller.php
+++ controladores/validacion_controller.php
@@ -17,14 +17,6 @@
             $campania = new Campania();
             $user = $campania->getUserByMac($mac);
             if(isset($user)) {
-<<<<<<< HEAD
-                $voucher = new Voucher();
-                if($voucher->validateVoucher($user->num_voucher) === 1) {
-                    return 'Location: vistas/banner.php';
-                } else {                
-                    return 'Location: vistas/error.php?e=error_voucher';
-                }
-=======
                 return 'Location: vistas/banner.php';
                 // $voucher = new Voucher();
                 // if($voucher->validateVoucher($user->num_voucher) == 1) {
@@ -32,7 +24,6 @@
                 // } else {                
                 //     return 'Location: vistas/error.php?e=error_voucher';
                 // }
->>>>>>> 49c2762c87e6fe5dbb4c6d28e1a70f8fac39d9ce
             } else {
                 return 'Location: vistas/formulario.php';
             }
